1bf5d8c67b147dd2eb7ec3fe949dc0d671f55b30
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-02-16 19:35:23 -0600

    Finish project

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ListadoComponent{
+  heroes: string[] = ['Spiderman', 'Ironman', 'Thor', 'Hulk','Captain America'];
+  heroeBorrado: string = '';
+  borrarHeroe(){
+    // this.heroes.shift()
+    // const heroeBorrado = this.heroes.shift();
+    this.heroeBorrado = this.heroes.shift() || '';
   }
-
 }
